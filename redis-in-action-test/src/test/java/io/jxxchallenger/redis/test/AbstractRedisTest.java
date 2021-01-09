5cd2fee810084f5ab11e301c62112cd4c0aa59ab
jxxchallenger <jxxchallenger@foxmail.com>
2021-01-09 17:47:55 +0800

    第三章 Redis命令

--- redis-in-action-test/src/test/java/io/jxxchallenger/redis/test/AbstractRedisTest.java
+++ redis-in-action-test/src/test/java/io/jxxchallenger/redis/test/AbstractRedisTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
@@ -12,14 +14,18 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(value = OrderAnnotation.class)
+@TestInstance(value = Lifecycle.PER_CLASS)
 public abstract class AbstractRedisTest {
 
     protected static RedisClient redisClient;
     
     protected StatefulRedisConnection<String, String> connection;
     
+    /**
+     * 测试类带有 @TestInstance(Lifecycle.PER_CLASS)注解，被@BeforeAll注解的方法无需声明为static
+     */
     @BeforeAll
-    public static void setup() {
+    public void setup() {
         redisClient = RedisClient.create("redis://123456789@localhost:6379/0");
     }
     
@@ -35,8 +41,9 @@ public abstract class AbstractRedisTest {
     
     @AfterEach
     public void closeConnection() {
-        this.connection.close();
         doClose();
+        this.connection.close();
+        
     }
     
     protected void doClose() {
