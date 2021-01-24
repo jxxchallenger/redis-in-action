e98c8de59d9d8e7ae333169e2eaf5f42db42918b
jxxchallenger <jxxchallenger@foxmail.com>
2021-01-24 18:18:04 +0800

    redis multi exec命令

---
+++ chapter03/src/test/java/io/jxxchallenger/redis/RedisTransationTest.java
@@ -0,0 +1,94 @@
+package io.jxxchallenger.redis;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import io.jxxchallenger.redis.test.AbstractRedisTest;
+import io.lettuce.core.api.sync.RedisCommands;
+
+public class RedisTransationTest extends AbstractRedisTest {
+
+    @Test
+    @Order(value = 0)
+    public void transation() {
+        RedisCommands<String, String> redisCommands = this.connection.sync();
+        String val = redisCommands.get("test");
+        System.out.println(val);
+        
+        SetThread setThread = new SetThread(redisCommands);
+        GetThread getThread = new GetThread(redisCommands);
+        Thread thread = new Thread(setThread, "set-thread");
+        
+        Thread thread2 = new Thread(getThread, "get-thread");
+        
+        thread.start();
+        thread2.start();
+        
+        try {
+            Thread.sleep(10 * 2000);
+            setThread.setRun(false);
+            getThread.setRun(false);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static class SetThread implements Runnable {
+        
+        private boolean run = true;
+        
+        private RedisCommands<String, String> redisCommands;
+        
+        public SetThread(RedisCommands<String, String> redisCommands) {
+            this.redisCommands = redisCommands;
+        }
+        
+        public void setRun(boolean run) {
+            this.run = run;
+        }
+
+        @Override
+        public void run() {
+            while(run) {
+                redisCommands.multi();
+                Long t = redisCommands.incr("test");
+                
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                redisCommands.decr("test");
+                redisCommands.exec();
+            }
+        }
+    }
+    
+    private static class GetThread implements Runnable {
+        
+        private boolean run = true;
+        
+        private RedisCommands<String, String> redisCommands;
+        
+        public GetThread(RedisCommands<String, String> redisCommands) {
+            this.redisCommands = redisCommands;
+        }
+        
+        public void setRun(boolean run) {
+            this.run = run;
+        }
+
+        @Override
+        public void run() {
+            while(run) {
+                System.out.println(redisCommands.get("test"));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
